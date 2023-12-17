1f1e1694ef998b96aeaf46c9ee52f3cbb6a90efc
Jayk5 <jayantkumar20@cse.iiitp.ac.in>
2023-12-17 17:22:10 +0530

    fix: multiple issues in board ui

--- ui/src/components/board.jsx
+++ ui/src/components/board.jsx
@@ -7,61 +7,61 @@ export default function Board() {
   const { isLogged, setIsLogged } = useContext(UserContext);
   const [board, setBoard] = useState([]);
   const [heads, setHeads] = useState([]);
-  // TODO - Fix this so each column's inputs are separate
-  const [item, setItem] = useState({
-    title: '',
-    description: '',
-  });
+  const [item, setItem] = useState({});
   const [newColumn, setNewColumn] = useState({
     title: '',
   });
   const navigate = useNavigate();
   const { id } = useParams();
 
-  useEffect(() => {
-    async function fetchData() {
-      const token = localStorage.getItem("token");
-      if (token) {
-        const serverResponse = await Axios.get(`http://localhost:8000/boards/${id}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        let cols = serverResponse.data.columns;
-        let sizeOfBoard = cols.length;
-        let maxSizeOfCol = 0;
-        for (let i = 0; i < sizeOfBoard; i++) {
-          if (cols[i].items.length > maxSizeOfCol) {
-            maxSizeOfCol = cols[i].items.length;
-          }
+  async function fetchData() {
+    try {
+      const serverResponse = await Axios.get(`http://localhost:8000/boards/${id}`);
+      let cols = serverResponse.data.columns;
+      let sizeOfBoard = cols.length;
+      let maxSizeOfCol = 0;
+      for (let i = 0; i < sizeOfBoard; i++) {
+        if (cols[i].items.length > maxSizeOfCol) {
+          maxSizeOfCol = cols[i].items.length;
         }
-        let newBoard = [];
-        let tempArr = []
-        for (let i = 0; i < sizeOfBoard; i++) {
-          let tempobj = {};
-          tempobj["id"] = cols[i].id;
-          tempobj["title"] = cols[i].title;
-          tempobj["position"] = cols[i].position;
-          tempArr.push(tempobj);
-        }
-        setHeads(tempArr);
-        for (let i = 0; i < maxSizeOfCol; i++) {
-          let tempArr = []
-          for (let j = 0; j < sizeOfBoard; j++) {
-            if (cols[j].items[i]) {
-              cols[j].items[i]["columnId"] = cols[j].id;
-              tempArr.push(cols[j].items[i]);
-            } else {
-              tempArr.push("");
-            }
-          }
-          newBoard.push(tempArr);
-        }
-        setBoard(newBoard);
-      } else {
-        navigate("/login");
       }
+      let newBoard = [];
+      let tempArr = []
+      let tempInputState = {};
+      for (let i = 0; i < sizeOfBoard; i++) {
+        let tempobj = {};
+        tempobj["id"] = cols[i].id;
+        tempobj["title"] = cols[i].title;
+        tempobj["position"] = cols[i].position;
+        tempInputState[cols[i].id] = {
+          title: '',
+          description: '',
+        };
+        tempArr.push(tempobj);
+      }
+      setItem(tempInputState);
+      setHeads(tempArr);
+      for (let i = 0; i < maxSizeOfCol; i++) {
+        let tempArr = []
+        for (let j = 0; j < sizeOfBoard; j++) {
+          if (cols[j].items[i]) {
+            cols[j].items[i]["columnId"] = cols[j].id;
+            tempArr.push(cols[j].items[i]);
+          } else {
+            tempArr.push("");
+          }
+        }
+        newBoard.push(tempArr);
+      }
+      setBoard(newBoard);
     }
+    catch (error) {
+      console.log(error);
+      navigate("/");
+    }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -75,7 +75,8 @@ export default function Board() {
           row[j] ? <>
             <Link to={`/items/${row[j].id}`}>{row[j].title}</Link>
             <br /> {row[j].description} <br />
-            <button onClick={() => handleItemDelete(row[j].id)}>Delete</button> </> : ""
+            {isLogged ? <button onClick={() => handleItemDelete(row[j].id)}>Delete</button> : ""}
+          </> : ""
         }</td>);
       }
       rows.push(<tr key={i}>{children}</tr>);
@@ -84,7 +85,7 @@ export default function Board() {
   }
 
   const handleItemAdd = (columnId) => {
-    const { title, description } = item;
+    const { title, description } = item[columnId];
     Axios.post(`http://localhost:8000/boards/${id}/${columnId}`, {
       title,
       description,
@@ -94,11 +95,14 @@ export default function Board() {
       },
     })
       .then((response) => {
-        setItem({
-          title: '',
-          description: '',
-        });
-        window.location.reload();
+        setItem((prevItem) => ({
+          ...prevItem,
+          [columnId]: {
+            title: '',
+            description: '',
+          }
+        }));
+        fetchData();
       })
       .catch((error) => {
         console.log(error);
@@ -106,11 +110,15 @@ export default function Board() {
       );
   };
 
-  const handleItemChange = (e) => {
+  const handleItemChange = (e, colId) => {
     const { name, value } = e.target;
+    const columnId = colId;
     setItem((prevItem) => ({
       ...prevItem,
-      [name]: value,
+      [columnId]: {
+        ...prevItem[columnId],
+        [name]: value,
+      }
     }));
   };
 
@@ -121,7 +129,7 @@ export default function Board() {
       },
     })
       .then((response) => {
-        window.location.reload();
+        fetchData();
       })
       .catch((error) => {
         console.log(error);
@@ -149,7 +157,7 @@ export default function Board() {
         setNewColumn({
           title: '',
         });
-        window.location.reload();
+        fetchData();
       })
       .catch((error) => {
         console.log(error);
@@ -164,7 +172,7 @@ export default function Board() {
       },
     })
       .then((response) => {
-        window.location.reload();
+        fetchData();
       })
       .catch((error) => {
         console.log(error);
@@ -186,53 +194,58 @@ export default function Board() {
             {heads.map((head) => (
               <th key={head.id}>{head.title}</th>
             ))}
-            <th>
-              <input
-                type="text"
-                name="title"
-                placeholder="Enter title"
-                value={newColumn.title}
-                onChange={handleColumnAddChange}
-              />
-              <button onClick={handleColumnAdd}>Add Column</button>
-            </th>
+            {isLogged ?
+              <th>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Enter title"
+                  value={newColumn.title}
+                  onChange={handleColumnAddChange}
+                />
+                <button onClick={handleColumnAdd}>Add Column</button>
+              </th> : ""}
           </tr>
         </thead>
         <tbody>
           {renderTable(board)}
         </tbody>
-        <tfoot>
-          <tr>
-            {heads.map((head) => (
-              <>
+        {isLogged ?
+          <tfoot>
+            <tr>
+              {heads.map((head) => (
+                <>
+                  <th>
+                    <input
+                      type="text"
+                      name="title"
+                      placeholder="Enter title"
+                      value={item[head.id].title}
+                      onChange={(e) => { handleItemChange(e, head.id) }}
+                    />
+                    <br />
+                    <input
+                      type="text"
+                      name="description"
+                      placeholder="Enter description"
+                      value={item[head.id].description}
+                      onChange={(e) => { handleItemChange(e, head.id) }}
+                    />
+                    <br />
+                    <button onClick={() => { handleItemAdd(head.id) }}>Add Item</button>
+                  </th>
+                </>
+              ))}
+            </tr>
+            <tr>
+              {heads.map((head) => (
                 <th>
-                  <input
-                    type="text"
-                    name="title"
-                    placeholder="Enter title"
-                    // value={item.title}
-                    onChange={handleItemChange}
-                  />
-                  <input
-                    type="text"
-                    name="description"
-                    placeholder="Enter description"
-                    // value={item.description}
-                    onChange={handleItemChange}
-                  />
-                  <button onClick={() => { handleItemAdd(head.id) }}>Add Item</button>
+                  <button onClick={() => { handleColumnDelete(head.id) }}>Delete Column</button>
                 </th>
-              </>
-            ))}
-          </tr>
-          <tr>
-            {heads.map((head) => (
-              <th>
-                <button onClick={() => { handleColumnDelete(head.id) }}>Delete Column</button>
-              </th>
-            ))}
-          </tr>
-        </tfoot>
+              ))}
+            </tr>
+          </tfoot>
+          : ""}
       </table>
     </>
   );
